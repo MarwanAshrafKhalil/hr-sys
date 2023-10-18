d5ebe202c12af8e07dd71354ec37624223b90380
MarwanAshrafKhalil <marwan.a.k.1997@gmailcom>
2023-10-18 17:33:48 +0300

    add redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,13 +1,26 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
-
+import {
+  combineReducers,
+  configureStore,
+  getDefaultMiddleware,
+} from "@reduxjs/toolkit";
 import employeeReducer from "./employee/employeeSlice";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+const persistCofnig = {
+  key: "root",
+  version: 1,
+  storage,
+};
 
+const rootReducer = combineReducers({ employee: employeeReducer });
+const persistedReducer = persistReducer(persistCofnig, rootReducer);
 export const store = configureStore({
-  reducer: {
-    employee: employeeReducer,
-  },
+  reducer: persistedReducer,
+
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
+
+export const persistor = persistStore(store);
